88fa7da123a321ca49d3ee4660d47bbc90252b66
LuciMito <lmcbmth@gmail.com>
2021-07-26 19:39:08 -0300

    Pulo duplo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     public float Speed;
     public float JumpForce;
+    public bool isJumping;
+    public bool doubleJump;
     private Rigidbody2D rig;
 
     // Start is called before the first frame update
@@ -32,7 +34,36 @@ public class Player : MonoBehaviour
     {
         if(Input.GetButtonDown("Jump"))
         {
-            rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if(!isJumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doubleJump = true;
+            }
+
+            else
+            {
+                if(doubleJump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce * 1.5f), ForceMode2D.Impulse);
+                    doubleJump = false;
+                }
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isJumping = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isJumping = true;
         }
     }
 
