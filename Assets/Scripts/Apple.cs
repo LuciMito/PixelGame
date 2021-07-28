a0d7361a1b6a8cb523f16aa9e44d30fb21a3948f
LuciMito <lmcbmth@gmail.com>
2021-07-28 01:31:53 -0300

    Adicionando coletáveis

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            Destroy(gameObject, 0.25f);
+        }
+    }
+}
