209cb841eb552b8e89859c52e17aa8d2a39e2571
LuciMito <lmcbmth@gmail.com>
2021-08-04 20:21:35 -0300

    Inimigo

---
+++ Assets/Scripts/MaskDude.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskDude : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    public float speed;
+
+    public Transform rightCol;
+    public Transform leftCol;
+
+    public Transform headPoint;
+
+    private bool colliding;
+
+    public LayerMask layer;
+
+    public BoxCollider2D boxCollider2D;
+    public CircleCollider2D circleCollider2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+        colliding = Physics2D.Linecast(rightCol.position, leftCol.position, layer);
+
+        if(colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            float height = col.contacts[0].point.y - headPoint.position.y;
+
+            if(height > 0)
+            {
+                col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+                speed = 0;
+                anim.SetTrigger("die");
+                boxCollider2D.enabled = false;
+                circleCollider2D.enabled = false;
+                rig.bodyType = RigidbodyType2D.Kinematic;
+                Destroy(gameObject, 0.33f);
+            }
+        }
+    }
+}
